0b390075ce62789d30d85ed3f3aef3c43226c3d1
mina <yegrinaaa@gmail.com>
2020-12-15 12:08:42 +0900

    ADD:로그인페이지 레이아웃 완료

--- src/Pages/Login/Login.js
+++ src/Pages/Login/Login.js
@@ -1,12 +1,51 @@
-import React, { Component } from "react";
+import React from "react";
+import "./Login.scss";
+import "../../Styles/reset.scss";
 
-class Login extends Component {
+class Login extends React.Component {
   constructor(props) {
     super(props);
-    this.state = {};
+    this.state = {
+      checkbox: false,
+    };
   }
+  IsChecked = () => {
+    this.setState({ checkbox: !this.state.checkbox });
+    console.log(this.state.checkbox);
+  };
+
   render() {
-    return <></>;
+    return (
+      <div className="Login">
+        <div className="section-login">
+          <p className="login-title">로그인</p>
+          <form>
+            <input type="text" placeholder="아이디를 입력해주세요" className="id box" />
+            <input type="password" placeholder="비밀번호를 입력해주세요" className="password box" />
+            <div className="check-and-find">
+              <div className="checkbox-wrap">
+                <input
+                  type="checkbox"
+                  id="security"
+                  onClick={this.IsChecked}
+                  checked={this.state.checkbox}
+                />
+                <label htmlFor="security">보안접속</label>
+              </div>
+              <div className="find-account">
+                <p>아이디 찾기</p>
+                <span></span>
+                <p>비밀번호 찾기</p>
+              </div>
+            </div>
+            <button className="login box">로그인</button>
+            <a href="#" className="login-to-assign">
+              회원가입
+            </a>
+          </form>
+        </div>
+      </div>
+    );
   }
 }
 

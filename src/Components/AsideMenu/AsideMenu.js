a321561332431da094a5827fe133262129bc25a4
Janghyeon Lee <janghyeon300@gmail.com>
2020-12-15 21:29:54 +0900

    ADD: AsideMenu 컴포넌트 기본 레이아웃 완료

--- src/Components/AsideMenu/AsideMenu.js
+++ src/Components/AsideMenu/AsideMenu.js
@@ -1,4 +1,5 @@
 import React, { Component } from "react";
+import "./AsideMenu.scss";
 
 class AsideMenu extends Component {
   constructor(props) {
@@ -6,7 +7,30 @@ class AsideMenu extends Component {
     this.state = {};
   }
   render() {
-    return <></>;
+    return (
+      <aisde className="AsideMenu">
+        <div className="dawn-delivery">
+          <p>샛별·택배</p>
+          <p className="delivery">배송안내</p>
+        </div>
+        <div className="menu">
+          <ul>
+            <li>등급별 혜택</li>
+            <li>레시피</li>
+            <li>베스트 후기</li>
+          </ul>
+        </div>
+        <div className="recently-seen-item">
+          <h2>최근 본 상품</h2>
+          <div className="recent-items">
+            <img src="images/tomato.jpg" alt="tomato"></img>
+            <img src="images/tomato.jpg" alt="tomato"></img>
+            <img src="images/tomato.jpg" alt="tomato"></img>
+            <img src="images/tomato.jpg" alt="tomato"></img>
+          </div>
+        </div>
+      </aisde>
+    );
   }
 }
 

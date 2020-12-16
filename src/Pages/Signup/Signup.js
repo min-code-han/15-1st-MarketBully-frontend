3be79233a56518cfc6cca2efbf82691f3067b34e
mina <yegrinaaa@gmail.com>
2020-12-16 17:00:29 +0900

    모달창 레이아웃 완료

--- src/Pages/Signup/Signup.js
+++ src/Pages/Signup/Signup.js
@@ -8,202 +8,262 @@ class Signup extends Component {
   }
   render() {
     return (
-      <div className="Signup">
-        <div className="signup-wrap">
-          <h1>회원가입</h1>
-          <p>
-            <i className="sub">*</i> 필수입력사항
-          </p>
-          <table className="info">
-            <tr>
-              <th>
-                아이디<i className="sub">*</i>
-              </th>
-              <td>
-                <input
-                  className="inputbox-commonstyle"
-                  type="text"
-                  placeholder="6자 이상의 영문 혹은 영문과 숫자를 조합"
-                />
-                <button className="button small-btn">중복확인</button>
-                <p className="guide">
-                  <span>&middot; 6자 이상의 영문 혹은 영문과 숫자를 조합</span>
-                  <span>&middot; 아이디 중복확인</span>
-                </p>
-              </td>
-            </tr>
-            <tr>
-              <th>
-                비밀번호<i className="sub">*</i>
-              </th>
-              <td>
-                <input
-                  className="inputbox-commonstyle"
-                  type="text"
-                  placeholder="6자 이상의 영문 혹은 영문과 숫자를 조합"
-                />
-                <p className="guide">
-                  <span>&middot; 10자 이상 입력</span>
-                  <span>&middot; 영문/숫자/특수문자(공백 제외)만 허용하며, 2개 이상 조합</span>
-                  <span>&middot; 동일한 숫자 3개 이상 연속 사용 불가</span>
-                </p>
-              </td>
-            </tr>
-            <tr>
-              <th>
-                비밀번호 확인<i className="sub">*</i>
-              </th>
-              <td>
-                <input
-                  className="inputbox-commonstyle"
-                  type="text"
-                  placeholder="비밀번호를 입력해주세요"
-                />
-                <p className="guide">
-                  <span>&middot; 동일한 비밀번호를 입력해주세요.</span>
-                </p>
-              </td>
-            </tr>
-            <tr>
-              <th>
-                이름<i className="sub">*</i>
-              </th>
-              <td>
-                <input
-                  className="inputbox-commonstyle"
-                  type="text"
-                  placeholder="비밀번호를 한번더 입력해주세요"
-                />
-              </td>
-            </tr>
-            <tr>
-              <th>
-                이메일<i className="sub">*</i>
-              </th>
-              <td>
-                <input
-                  className="inputbox-commonstyle"
-                  type="text"
-                  placeholder="예:marketbully@kurly.com"
-                />
-                <button className="button small-btn">중복확인</button>
-              </td>
-            </tr>
-            <tr>
-              <th>
-                휴대폰<i className="sub">*</i>
-              </th>
-              <td>
-                <input
-                  className="inputbox-commonstyle"
-                  type="text"
-                  placeholder="숫자만 입력해주세요"
-                />
-                <button className="button gray-btn">인증번호 받기</button>
-              </td>
-            </tr>
-            <tr>
-              <th>
-                주소<i className="sub">*</i>
-              </th>
-              <td>
-                <button className="big-btn">
-                  <i class="fas fa-search"></i>주소검색
-                </button>
-                <p className="guide">
-                  <span>배송지에 따라 상품 정보가 달라질 수 있습니다.</span>
-                </p>
-              </td>
-            </tr>
-            <tr>
-              <th>성별</th>
-              <td className="fw-400">
-                <label htmlFor="man">
-                  <span className="radio-ico"></span>
-                  <input type="radio" id="man" name="gender" />
-                  남자
-                </label>
-                <label htmlFor="woman">
-                  <span className="radio-ico"></span>
-                  <input type="radio" id="woman" name="gender" />
-                  여자
-                </label>
-                <label htmlFor="none">
-                  <span className="radio-ico">
-                    <span className="radio-checked"></span>
-                  </span>
-                  <input type="radio" id="none" name="gender" />
-                  선택안함
-                </label>
-              </td>
-            </tr>
-            <tr>
-              <th>생년월일</th>
-              <td>
-                <div className="birth-input-wrap">
-                  <input type="text" placeholder="YYYY" />
-                  <span>/</span>
-                  <input type="text" placeholder="MM" />
-                  <span>/</span>
-                  <input type="text" placeholder="DD" />
-                </div>
-                <p className="guide">
-                  <span className="red">x 태어난 월을 정확하게 입력해주세요.</span>
-                </p>
-              </td>
-            </tr>
-            <tr>
-              <th>추가입력 사항</th>
-              <td className="fw-400">
-                <label htmlFor="id">
-                  <span className="radio-ico"></span>
-                  <input type="radio" id="id" name="recommend" />
-                  추천인 아이디
-                </label>
-                <label htmlFor="event">
-                  <span className="radio-ico"></span>
-                  <input type="radio" id="event" name="recommend" />
-                  참여 이벤트명
-                </label>
-                <div className="joinevent">
+      <>
+        <div className="Signup">
+          <div className="signup-wrap">
+            <h1>회원가입</h1>
+            <p>
+              <i className="sub">*</i> 필수입력사항
+            </p>
+            <table className="info">
+              <tr>
+                <th>
+                  아이디<i className="sub">*</i>
+                </th>
+                <td>
                   <input
+                    className="inputbox-commonstyle"
                     type="text"
-                    placeholder="참여 이벤트명을 입력해주세요"
-                    className="inputbox-commonstyle mt-24"
+                    placeholder="6자 이상의 영문 혹은 영문과 숫자를 조합"
                   />
-                  <div className="event-txt">
-                    <p className="guide">
-                      <span>추천인 아이디와 참여 이벤트면 중 하나만 선택 가능합니다.</span>
-                      <span>가입 이후, 수정이 불가능합니다.</span>
-                      <span>대소문자 및 띄어쓰기에 유의해주세요</span>
-                    </p>
+                  <button className="button small-btn">중복확인</button>
+                  <p className="guide">
+                    <span>&middot; 6자 이상의 영문 혹은 영문과 숫자를 조합</span>
+                    <span>&middot; 아이디 중복확인</span>
+                  </p>
+                </td>
+              </tr>
+              <tr>
+                <th>
+                  비밀번호<i className="sub">*</i>
+                </th>
+                <td>
+                  <input
+                    className="inputbox-commonstyle"
+                    type="text"
+                    placeholder="6자 이상의 영문 혹은 영문과 숫자를 조합"
+                  />
+                  <p className="guide">
+                    <span>&middot; 10자 이상 입력</span>
+                    <span>&middot; 영문/숫자/특수문자(공백 제외)만 허용하며, 2개 이상 조합</span>
+                    <span>&middot; 동일한 숫자 3개 이상 연속 사용 불가</span>
+                  </p>
+                </td>
+              </tr>
+              <tr>
+                <th>
+                  비밀번호 확인<i className="sub">*</i>
+                </th>
+                <td>
+                  <input
+                    className="inputbox-commonstyle"
+                    type="text"
+                    placeholder="비밀번호를 입력해주세요"
+                  />
+                  <p className="guide">
+                    <span>&middot; 동일한 비밀번호를 입력해주세요.</span>
+                  </p>
+                </td>
+              </tr>
+              <tr>
+                <th>
+                  이름<i className="sub">*</i>
+                </th>
+                <td>
+                  <input
+                    className="inputbox-commonstyle"
+                    type="text"
+                    placeholder="비밀번호를 한번더 입력해주세요"
+                  />
+                </td>
+              </tr>
+              <tr>
+                <th>
+                  이메일<i className="sub">*</i>
+                </th>
+                <td>
+                  <input
+                    className="inputbox-commonstyle"
+                    type="text"
+                    placeholder="예:marketbully@kurly.com"
+                  />
+                  <button className="button small-btn">중복확인</button>
+                </td>
+              </tr>
+              <tr>
+                <th>
+                  휴대폰<i className="sub">*</i>
+                </th>
+                <td>
+                  <input
+                    className="inputbox-commonstyle"
+                    type="text"
+                    placeholder="숫자만 입력해주세요"
+                  />
+                  <button className="button gray-btn">인증번호 받기</button>
+                </td>
+              </tr>
+              <tr>
+                <th>
+                  주소<i className="sub">*</i>
+                </th>
+                <td>
+                  <button className="big-btn">
+                    <i class="fas fa-search"></i>주소검색
+                  </button>
+                  <p className="guide">
+                    <span>배송지에 따라 상품 정보가 달라질 수 있습니다.</span>
+                  </p>
+                </td>
+              </tr>
+              <tr>
+                <th>성별</th>
+                <td className="fw-400">
+                  <input type="radio" id="man" name="gender" />
+                  <label htmlFor="man">남자</label>
+                  <input type="radio" id="woman" name="gender" />
+                  <label htmlFor="woman">여자</label>
+                  <label htmlFor="none">
+                    <span className="radio-ico">
+                      <span className="radio-checked"></span>
+                    </span>
+                    <input type="radio" id="none" name="gender" />
+                    선택안함
+                  </label>
+                </td>
+              </tr>
+              <tr>
+                <th>생년월일</th>
+                <td>
+                  <div className="birth-input-wrap">
+                    <input type="text" placeholder="YYYY" maxlength="4" />
+                    <span>/</span>
+                    <input type="text" placeholder="MM" maxlength="2" />
+                    <span>/</span>
+                    <input type="text" placeholder="DD" maxlength="2" />
                   </div>
-                </div>
-              </td>
-            </tr>
-          </table>
-          <div className="under-line"></div>
+                  <p className="guide">
+                    <span className="red">x 태어난 월을 정확하게 입력해주세요.</span>
+                  </p>
+                </td>
+              </tr>
+              <tr>
+                <th>추가입력 사항</th>
+                <td className="fw-400">
+                  <input type="radio" id="id" name="recommend" />
+                  <label htmlFor="id">추천인 아이디</label>
+                  <input type="radio" id="event" name="recommend" />
+                  <label htmlFor="event">참여 이벤트명</label>
+                  <div className="joinevent">
+                    <input
+                      type="text"
+                      placeholder="참여 이벤트명을 입력해주세요"
+                      className="inputbox-commonstyle mt-24"
+                    />
+                    <div className="event-txt">
+                      <p className="guide">
+                        <span>추천인 아이디와 참여 이벤트면 중 하나만 선택 가능합니다.</span>
+                        <span>가입 이후, 수정이 불가능합니다.</span>
+                        <span>대소문자 및 띄어쓰기에 유의해주세요</span>
+                      </p>
+                    </div>
+                  </div>
+                </td>
+              </tr>
+            </table>
+            <div className="under-line"></div>
+            <table className="agree">
+              <tr>
+                <th>
+                  이용약관동의<i className="sub">*</i>
+                </th>
+                <td>
+                  <div className="agree-txt">
+                    <div className="ageree-check">
+                      <input type="radio" id="all" />
+                      <label htmlFor="all" className="check-all">
+                        전체 동의합니다
+                      </label>
+                      <p className="guide">
+                        <span>
+                          선택항목에 동의하지 않은 경우도 회원가입 및 일반적인 서비스를 이용할 수
+                          있습니다.
+                        </span>
+                      </p>
+                    </div>
+                    <div className="ageree-check">
+                      <input type="radio" id="use" />
+                      <label htmlFor="use">
+                        이용약관 동의<span className="necessary">(필수)</span>
+                      </label>
+                      <p className="purple-txt see-more">
+                        약관보기<i class="fas fa-chevron-right see-more-ico"></i>
+                      </p>
+                    </div>
+                    <div className="ageree-check">
+                      <input type="radio" id="personal" />
+                      <label htmlFor="personal">
+                        개인정보처리방침 동의<span className="necessary">(필수)</span>
+                      </label>
+                      <p className="purple-txt see-more">
+                        약관보기<i class="fas fa-chevron-right see-more-ico"></i>
+                      </p>
+                    </div>
+                    <div className="ageree-check">
+                      <input type="radio" id="personal" />
+                      <label htmlFor="personal">
+                        개인정보처리방침 동의<span className="necessary">(필수)</span>
+                      </label>
+                      <p className="purple-txt see-more">
+                        약관보기<i class="fas fa-chevron-right see-more-ico"></i>
+                      </p>
+                    </div>
+                    <div className="ageree-check">
+                      <input type="radio" id="coupon" />
+                      <label htmlFor="coupon">
+                        무료배송, 할인쿠폰 등 혜택/정보 수신 동의
+                        <span className="necessary">(선택)</span>
+                      </label>
+                      <div className="check-news">
+                        <input type="radio" id="sms" />
+                        <label htmlFor="sms">SMS</label>
+                        <input type="radio" id="email" />
+                        <label htmlFor="email">이메일</label>
+                        <p className="purple-txt if-you-agree">
+                          <span className="detail"></span>
+                          동의 시 한 달간[5% 적립]+[무제한 무료배송]
+                          <span className="necessary">(첫 주문 후 적용)</span>
+                        </p>
+                      </div>
+                    </div>
+                    <div className="ageree-check">
+                      <input type="radio" id="personal" />
+                      <label htmlFor="personal">
+                        본인은 만 14세 이상입니다.<span className="necessary">(필수)</span>
+                      </label>
+                    </div>
+                  </div>
+                </td>
+              </tr>
+            </table>
+            <button className="make-account">가입하기</button>
+          </div>
         </div>
-        <div className="agree">
-          <table>
-            <tr>
-              <th>
-                이용약관동의<i className="sub">*</i>
-              </th>
-            </tr>
-            <td>
-              <div className="agree-txt">
-                <div></div>
-                <div></div>
-                <div></div>
-                <div></div>
-                <div></div>
-                <div></div>
-              </div>
-            </td>
-          </table>
+        <div className="Signup-modal">
+          <div className="modal-box">
+            <div className="alert">
+              <p>알림메세지</p>
+              <button>
+                <i class="fas fa-times"></i>
+              </button>
+            </div>
+            <p>아이디를 입력해 주세요</p>
+            <div className="modal-btn">
+              <button>확인</button>
+            </div>
+          </div>
         </div>
-      </div>
+      </>
     );
   }
 }

aaeea5f7af4ce1615d85b406c761c43be512a5e0
unknown <xpmxf5@gmail.com>
2020-12-16 16:08:00 +0900

    git 리뷰사항들 적용 완료

--- src/Components/ItemCard/ItemCard.js
+++ src/Components/ItemCard/ItemCard.js
@@ -9,21 +9,13 @@ class ItemCard extends Component {
   render() {
     const { name, price, imgUrl } = this.props;
     return (
-      <>
-        <div className="ItemCard">
-          <img src={imgUrl} alt="삼겹살 먹고 싶띠" />
-          <div className="header">{name}</div>
-          <div className="price">{price}</div>
-        </div>
-      </>
+      <div className="ItemCard">
+        <img src={imgUrl} alt="제품의 이미지" />
+        <div className="header">{name}</div>
+        <div className="price">{price}</div>
+      </div>
     );
   }
 }
 
 export default ItemCard;
-
-// a 태그는 각자 알아서 걸어야 할듯. 페이지마다 걸리는 게 다르다.
-// a 태그 눌러도 색깔 변화나, underline은 안 나오게 scss 조정 해둠.
-// 내일 회의에서 얘기해야 할듯
-// 다른 사람들은 img, header, price 까지만 필요
-// font-family : noto sans , font-size  적용함

412a1c1fc5a2f32d3f8dd47615c3956e6eb0ca08
Yoooong-Chan <rhdlwmal1234@naver.com>
2020-12-16 15:43:34 +0900

    Header 초기 레이아웃 리뷰 수정 완료

--- src/Components/Header/Header.js
+++ src/Components/Header/Header.js
@@ -9,55 +9,53 @@ class Header extends Component {
   }
   render() {
     return (
-      <>
-        <div className="header">
-          <ul className="headerMenu">
+      <div className="header">
+        <ul className="headerMenu">
+          <li>
+            <Link to="/">회원가입</Link>
+          </li>
+          <li>
+            <Link to="/">로그인</Link>
+          </li>
+          <li>
+            <Link to="/"> 고객센터</Link>
+          </li>
+          <li>
+            <Link to="/">배송지역 검색</Link>
+          </li>
+        </ul>
+
+        <div className="header__logo">
+          <img src="https://res.kurly.com/pc/service/common/1908/delivery_190819.gif"></img>
+          <img src="https://res.kurly.com/images/marketkurly/logo/logo_x2.png"></img>
+        </div>
+
+        <div className="gnb">
+          <ul>
             <li>
-              <Link to="/">회원가입</Link>
+              <Link to="/">전체 카테고리</Link>
             </li>
             <li>
-              <Link to="/">로그인</Link>
+              <Link to="/">신상품</Link>
             </li>
             <li>
-              <Link to="/"> 고객센터</Link>
+              <Link to="/">베스트</Link>
             </li>
             <li>
-              <Link to="/">배송지역 검색</Link>
+              <Link to="/">알뜰쇼핑</Link>
+            </li>
+            <li>
+              <Link to="/">이벤트</Link>
             </li>
           </ul>
-
-          <div className="header__logo">
-            <img src="https://res.kurly.com/pc/service/common/1908/delivery_190819.gif"></img>
-            <img src="https://res.kurly.com/images/marketkurly/logo/logo_x2.png"></img>
+          <div className="gnb__search">
+            <input />
           </div>
-
-          <div className="gnb">
-            <ul>
-              <li>
-                <Link to="/">전체 카테고리</Link>
-              </li>
-              <li>
-                <Link to="/">신상품</Link>
-              </li>
-              <li>
-                <Link to="/">베스트</Link>
-              </li>
-              <li>
-                <Link to="/">알뜰쇼핑</Link>
-              </li>
-              <li>
-                <Link to="/">이벤트</Link>
-              </li>
-            </ul>
-            <div className="gnb__search">
-              <input />
-            </div>
-            <div className="gnb__cart">
-              <img src="https://res.kurly.com/pc/ico/1908/ico_cart_x2_v2.png"></img>
-            </div>
+          <div className="gnb__cart">
+            <img src="https://res.kurly.com/pc/ico/1908/ico_cart_x2_v2.png"></img>
           </div>
         </div>
-      </>
+      </div>
     );
   }
 }
